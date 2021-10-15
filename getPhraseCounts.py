faf45f4a032e1cb8b6e27806dfe483a9708c0591
Srinivasa Chivaluri <spc6ph@virginia.edu>
2021-10-14 23:21:25 -0400

    changed
    changed

--- getPhraseCounts.py
+++ getPhraseCounts.py
@@ -7,6 +7,7 @@
 
 import tokenize
 import nltk
+from rdfHandler import rdfObject
 
 def test():
     phraseList = ["Apple", "Pear", "Peach", "Banana"]
@@ -16,69 +17,72 @@ def test():
 
     return(phraseDictionary)
 
-def getPhraseCount():
-    #fileObject = open(r'C:\Users\srini\UVA-MSDS\DS-6011-CAP\Files\AI08_2016.txt', 'r')
+def getPhraseCount(phraseList=[],tokenList=[]):
+    countList = [0]*len(phraseList)
+
+    if (len(phraseList) == 0 | len(tokenList) == 0):
+        return(dict(zip(phraseList)))
+
+    print("*** BEGIN SOURCE PHRASES ***")
+    for tempPhrase in phraseList:
+        print("Source Phrase : " + tempPhrase)
+    print("*** END SOURCE PHRASES ***")
+
+    phrasePosition = 0
+    for tempPhrase in phraseList:
+        print("Source Phrase : " + tempPhrase)
+
+        phrase_Tokens = nltk.word_tokenize(tempPhrase)
+
+        print(phrase_Tokens)
+        for i in range(0,len(phrase_Tokens),1):
+            print("Token # {0} Word : {1}".format(i,phrase_Tokens[i]))
+
+
+        tPosition = 0
+        while tPosition < len(tokenList)-len(phrase_Tokens):
+            compPhrase = ""
+            pCount = 0
+            matched = False
+            while pCount < len(phrase_Tokens):
+                compPhrase = compPhrase + tokenList[tPosition + pCount]
+                if phrase_Tokens[pCount] == tokenList[tPosition+pCount]:
+                    matched = True
+                else:
+                    matched = False
+                pCount = pCount + 1
+            #print("compPhrase : " + compPhrase)
+
+            #if matched == True:
+            if compPhrase == tempPhrase:
+                print("*** MATCHED ***")
+
+                print("tempPhrase : " + tempPhrase)
+                print("compPhrase : " + compPhrase)
+                countList[phrasePosition] = countList[phrasePosition] + 1
+
+            tPosition = tPosition + 1
+        phrasePosition = phrasePosition + 1
+    return (dict(zip(phraseList, countList)))
+
+if __name__ == '__main__':
+    rdf = rdfObject('https://mikeanders.org/data/Ontologies/DoD/DASD SKOS_Ontology.rdf', 'web')
+
+    print(type(rdf.synonymsList()))
+    synonymsList = rdf.synonymsList()
+    #phCount = test()
+    #print(phCount)
+
+#    fileObject = open(r'C:\Users\srini\UVA-MSDS\DS-6011-CAP\Files\AI08_2016.txt', 'r')
     fileObject = open(r'C:\Users\srini\UVA-MSDS\DS-6011-CAP\Files\A088P.TXT', 'r')
 
     data = fileObject.read()
-    data.replace(r"\n"," ")
-    #print(data)
+    data.replace(r"\n", " ")
+    # print(data)
     nltk_tokens = nltk.word_tokenize(data)
-    '''
-    print(type(nltk_tokens))
-    for i in range(0,len(nltk_tokens),1):
-        print("Token # {0} Word : {1}".format(i,nltk_tokens[i]))
-    '''
-    tempPhrase = "National Capital Region"
-    print(len(tempPhrase))
 
-    phrase_Tokens = nltk.word_tokenize(tempPhrase)
+    retDict = getPhraseCount(synonymsList,nltk_tokens)
+    print(retDict)
 
-    for i in range(0,len(phrase_Tokens),1):
-        print("Token # {0} Word : {1}".format(i,phrase_Tokens[i]))
-
-    print(len(phrase_Tokens))
-
-    tPosition = 0
-    while tPosition < len(nltk_tokens):
-        compPhrase = ""
-        pCount = 0
-        matched = False
-        while pCount < len(phrase_Tokens):
-            compPhrase = compPhrase + nltk_tokens[tPosition + pCount]
-            if phrase_Tokens[pCount] == nltk_tokens[tPosition+pCount]:
-                matched = True
-            else:
-                matched = False
-            pCount = pCount + 1
-
-        if matched == True:
-            print("*** MATCHED ***")
-            print("compPhrase : " + compPhrase)
-        tPosition = tPosition + 1
-        '''
-        compPhrase = compPhrase + nltk_tokens[tPosition+pCount]
-        if (tempPhrase.strip() == compPhrase.strip()):
-            print("*** MATCHED *** : " + tempPhrase + " == " + compPhrase)
-        print("tempPhrase : " + tempPhrase)
-        print("compPhrase : " + compPhrase)
-        '''
-
-    #print(nltk_tokens.size())
-    # with open(r'C:\Users\srini\UVA-MSDS\DS-6011-CAP\Files\AI08_2016.txt', 'rb') as f:
-    #     #tokens = tokenize.tokenize(f.readline)
-    #     #for token in tokens:
-    #
-    #     #    print("token : " + token. + token.string)
-    #     #print(f.readline)
-    #     #nltk_tokens = nltk.word_tokenize(f.readline)
-    #     #print(nltk_tokens.count())
-    #     data = f.read() #.replace(r"\\n", "")
-    #     nltk_tokens = nltk.word_tokenize(data)
-    #     print(nltk_tokens )
-
-if __name__ == '__main__':
-    phCount = test()
-    print(phCount)
-
-    getPhraseCount()
+    rdf = rdfObject('https://mikeanders.org/data/Ontologies/DoD/DASD SKOS_Ontology.rdf', 'web')
+    print(rdf.synonymsList())
