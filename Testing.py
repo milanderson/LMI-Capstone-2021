3a945c0e46281af02fc0cd924bbc5b0efce5c94d
Srinivasa Chivaluri <spc6ph@virginia.edu>
2021-10-27 07:41:09 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -11,6 +11,57 @@ def pltHistogram():
     plt.bar(dict1.keys(), dict1.values(), width=.5, color='g')
     plt.show()
 
+class Concept():
+    prefLables = {}
+    altLabels = {}
+    acronyms = {}
+    synonyms = {}
+    antonyms = {}
+    broaders = {}
+    narrowers = {}
+    related = {}
+    about = ""
+    def __init__(self,aboutText=None):
+        if aboutText == None:
+            about = ""
+        else:
+            about = aboutText
+        prefLables = {}
+        altLabels = {}
+        acronyms = {}
+        synonyms = {}
+        antonyms = {}
+        broaders = {}
+        narrowers = {}
+        related = {}
+
+    def addPrefLabel(self,labelText,labelText):
+        self.prefLables[labelText] = labelText
+
+    def addAltLabel(self,labelText,labelText):
+        self.altLabels[labelText] = labelText
+
+    def addAcronyms(self,labelText,labelText):
+        self.acronyms[labelText] = labelText
+
+    def addSynonyms(self,labelText,labelText):
+        self.synonyms[labelText] = labelText
+
+    def addAntonyms(self,labelText,labelText):
+        self.antonyms[labelText] = labelText
+
+    def addBroaders(self,labelText,labelText):
+        self.broaders[labelText] = labelText
+
+    def addNarrowers(self,labelText,labelText):
+        self.narrowers[labelText] = labelText
+
+    def addRalted(self,labelText,labelText):
+        self.related[labelText] = labelText
+
+
+
+
 if __name__ == '__main__':
     #pltHistogram()
     headers = {
@@ -22,22 +73,43 @@ if __name__ == '__main__':
     #print(xmlRDFString)
     print([f.string for f in xmlRDFString.find_all('prefLabel')])
 
+
+    conceptList = []
+
     for pref in xmlRDFString.find_all('Concept'):
 
         print("*** CONCEPT *** : " + pref.attrs['rdf:about'])
         print("==================")
         print("prefLable : " + pref.find("prefLabel").text)
+        #print("altLable : " + pref.find("altLabel").text)
+        #print("acronym : " + pref.find("acronym").text)
 
         # pref.find_all("synonym")[0].text
+        conceptObj = Concept()
+
+        print("altLable->")
+        for item in pref.find_all('altLable'):
+            print(item.text)
+        print("acronym->")
+        for item in pref.find_all('acronym'):
+            print(item.text)
+
         print("Synonyms->")
         for item in pref.find_all('synonym'):
             print(item.text)
-        print("Synonyms->")
-        for item in pref.find_all('synonym'):
+        print("Broader->")
+        for item in pref.find_all('broader'):
             print(item.text)
-        print("Synonyms->")
-        for item in pref.find_all('synonym'):
+        print("Narrower->")
+        for item in pref.find_all('narrower'):
             print(item.text)
+        print("Antonyms->")
+        for item in pref.find_all('antonyms'):
+            print(item.text)
+        print("Related->")
+        for item in pref.find_all('related'):
+            print(item.text)
+
 
 '''
     synonymsList = xmlRDFString.find_all('synonym')
